240806c715bc15279be70aeb8e70a0c9c6a357fc
ximik777 <ya@ya.gs>
2016-11-03 11:53:44 +0300

    fix Core

--- src/Autoload.php
+++ src/Autoload.php
@@ -4,12 +4,8 @@ namespace VX\Core;
 
 class Autoload
 {
-    public static $vendor;
-
     public static function Register($prepend = false)
     {
-        self::$vendor = substr(__DIR__, 0, strrpos(__DIR__, strtr('/'.__NAMESPACE__, '\\', '/')));
-
         if (PHP_VERSION_ID < 50300) {
             spl_autoload_register(array(__CLASS__, 'Autoload'));
         } else {
@@ -19,12 +15,7 @@ class Autoload
 
     public static function Autoload($class)
     {
-
-        if (0 !== strpos($class, 'Twig')) {
-            $file = ((strncmp($class, 'bundle', 6) === 0) ? DR : self::$vendor) . '/' . strtr($class, '\\', '/') . '.php';
-        } else {
-            $file = self::$vendor.'/Twig/lib/'.str_replace(array('_', "\0"), array('/', ''), $class).'.php';
-        }
+        $file = strtr(__DIR__ . str_replace(__NAMESPACE__, "", $class), '\\', '/') . '.php';
 
         if (is_file($file))
             require_once($file);
